c65a5fbcb71303233444cad1a0361223759a452f
plamenivanov91 <mrplamenivanov91@gmail.com>
2024-03-07 09:35:43 +0000

    Deploying to gh-pages from @ plamenivanov91/ui5-webcomponents@30763ffee896d691d6d1d2d65900c47458293eda 🚀

---
+++ preview/local-cdn/base/dist/generated/VersionInfo.js
@@ -0,0 +1,11 @@
+const VersionInfo = {
+    version: "1.23.1-rc.0",
+    major: 1,
+    minor: 23,
+    patch: 1,
+    suffix: "-rc.0",
+    isNext: false,
+    buildTime: 1709803891,
+};
+export default VersionInfo;
+//# sourceMappingURL=VersionInfo.js.map
