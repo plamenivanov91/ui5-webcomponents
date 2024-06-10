82870c0f9886fe6598a4429a5a7fb53fbc34d381
plamenivanov91 <plamenivanov91@users.noreply.github.com>
2024-06-10 01:04:38 +0000

    Deploying to gh-pages from @ plamenivanov91/ui5-webcomponents@791653e253faae4847f026d022d0ddcd47a96666 🚀

---
+++ nightly/assets/test/pages/ComponentStyles.html-84234094.js
@@ -0,0 +1,1 @@
+import"../../bundle.esm-1dec7cf3.js";const n="_inputIcon_1ojd7_2";const o=document.getElementById("custom-icon");o.classList.add(n);
---
+++ nightly/assets/test/pages/Icon.html-2db996d9.js
@@ -0,0 +1,1 @@
+import"../../bundle.esm-1dec7cf3.js";(async()=>{var a=await window["sap-ui-webcomponents-bundle"].getIconNames();a.forEach(e=>{var n=document.createElement("ui5-icon");n.name=e,allIcons.appendChild(n)})})();
