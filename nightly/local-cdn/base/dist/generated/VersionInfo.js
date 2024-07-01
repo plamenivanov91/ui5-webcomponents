6a61aafab4b2fecd6e6c1cf6525945a8bcbe6cf3
plamenivanov91 <plamenivanov91@users.noreply.github.com>
2024-07-01 01:10:31 +0000

    Deploying to gh-pages from @ plamenivanov91/ui5-webcomponents@740d30eee11439225191503a9ff73eb137af8c76 🚀

--- nightly/local-cdn/base/dist/generated/VersionInfo.js
+++ nightly/local-cdn/base/dist/generated/VersionInfo.js
@@ -5,7 +5,7 @@ const VersionInfo = {
     patch: 0,
     suffix: "-rc.6",
     isNext: false,
-    buildTime: 1719709502,
+    buildTime: 1719796001,
 };
 export default VersionInfo;
 //# sourceMappingURL=VersionInfo.js.map
---
+++ nightly/local-cdn/main/dist/assets/test/pages/ComponentStyles.html-b45cc2a7.js
@@ -0,0 +1,1 @@
+import"../../bundle.esm-1009cacb.js";const n="_inputIcon_1wpmw_2";const o=document.getElementById("custom-icon");o.classList.add(n);
