a13f1660719500e6df6035394f32e7d9a6ba51cf
plamenivanov91 <plamenivanov91@users.noreply.github.com>
2024-10-30 01:15:16 +0000

    Deploying to gh-pages from @ plamenivanov91/ui5-webcomponents@56ad3115affe17f453a30ac0880ac879dbbd763b 🚀

---
+++ nightly/assets/test/pages/Icon.html-DaKvCyC-.js
@@ -0,0 +1,1 @@
+import"../../bundle.esm-B5_ASXMl.js";(async()=>{var a=await window["sap-ui-webcomponents-bundle"].getIconNames();a.forEach(e=>{var n=document.createElement("ui5-icon");n.name=e,allIcons.appendChild(n)})})();
