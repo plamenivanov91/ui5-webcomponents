5ca65ad62215143c446c6aaa17f306304f0d2b27
plamenivanov91 <plamenivanov91@users.noreply.github.com>
2024-11-03 01:19:03 +0000

    Deploying to gh-pages from @ plamenivanov91/ui5-webcomponents@7a7d00c0d615283d1a86d775b6c51db7feadfb42 🚀

--- nightly/local-cdn/base/dist/generated/VersionInfo.js
+++ nightly/local-cdn/base/dist/generated/VersionInfo.js
@@ -5,7 +5,7 @@ const VersionInfo = {
     patch: 0,
     suffix: "-rc.4",
     isNext: false,
-    buildTime: 1730509767,
+    buildTime: 1730596513,
 };
 export default VersionInfo;
 //# sourceMappingURL=VersionInfo.js.map
--- nightly/local-cdn/main/dist/assets/test/pages/Icon.html-B5WUAiCZ.js
+++
@@ -1,1 +0,0 @@
-import"../../bundle.esm-dmnW36Ck.js";(async()=>{var a=await window["sap-ui-webcomponents-bundle"].getIconNames();a.forEach(e=>{var n=document.createElement("ui5-icon");n.name=e,allIcons.appendChild(n)})})();
