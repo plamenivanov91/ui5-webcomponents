93cb3a680b686b53e0f59c28c459958b4d05ff89
plamenivanov91 <plamenivanov91@users.noreply.github.com>
2025-04-21 01:25:27 +0000

    Deploying to gh-pages from @ plamenivanov91/ui5-webcomponents@d07b0891ef3f3e6c60354e07c9c12433ce1f14d5 🚀

---
+++ nightly/local-cdn/ai/dist/assets/ListItemStandardExpandableTextTemplate-B1R3zuIL.js
@@ -0,0 +1,1 @@
+import{a as x,E as m}from"./bundle.esm-KX55g8MQ.js";function p(a){const{className:t,text:e,maxCharacters:r,part:s}=a;return x(m,{part:s,class:t,text:e,maxCharacters:r})}export{p as default};
--- nightly/local-cdn/main/dist/assets/test/pages/Icon.html-bdxckypX.js
+++
@@ -1,1 +0,0 @@
-import"../../bundle.esm-CaOG0eWD.js";(async()=>{var a=await window["sap-ui-webcomponents-bundle"].getIconNames();a.forEach(e=>{var n=document.createElement("ui5-icon");n.name=e,allIcons.appendChild(n)})})();
