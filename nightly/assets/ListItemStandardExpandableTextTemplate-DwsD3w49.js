974b24a0b000abe6ac95c245b3e0ac27649ef2f5
plamenivanov91 <plamenivanov91@users.noreply.github.com>
2025-10-25 01:20:13 +0000

    Deploying to gh-pages from @ plamenivanov91/ui5-webcomponents@3c9d9780c53818828a7cd5dcced6f8ddde9270be 🚀

---
+++ nightly/assets/ListItemStandardExpandableTextTemplate-DwsD3w49.js
@@ -0,0 +1,1 @@
+import{a as x,E as m}from"./bundle.esm-CeYOpOvR.js";function p(a){const{className:t,text:e,maxCharacters:r,part:s}=a;return x(m,{part:s,class:t,text:e,maxCharacters:r})}export{p as default};
