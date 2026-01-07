41c3ba12b6a217378f501e7a74568121fe60f75c
plamenivanov91 <plamenivanov91@users.noreply.github.com>
2026-01-07 01:33:57 +0000

    Deploying to gh-pages from @ plamenivanov91/ui5-webcomponents@2f236870c6768e5744a14568b239eea9cc7bdb30 🚀

---
+++ nightly/assets/test/pages/theming/Themes2.html-DDSp0ruX.js
@@ -0,0 +1,1 @@
+import"../../../bundle.esm-CQ4MTkNr.js";setTimeout(()=>{window["sap-ui-webcomponents-bundle"].configuration.setTheme("sap_horizon_hcb")},1e3);
--- nightly/local-cdn/base/dist/generated/VersionInfo.js
+++ nightly/local-cdn/base/dist/generated/VersionInfo.js
@@ -5,7 +5,7 @@ const VersionInfo = {
     patch: 0,
     suffix: "-rc.4",
     isNext: false,
-    buildTime: 1767662948,
+    buildTime: 1767749363,
 };
 export default VersionInfo;
 //# sourceMappingURL=VersionInfo.js.map
