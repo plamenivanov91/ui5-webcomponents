c26377e1dc422392f2824198dcccd168b8880952
plamenivanov91 <plamenivanov91@users.noreply.github.com>
2026-02-18 02:06:57 +0000

    Deploying to gh-pages from @ plamenivanov91/ui5-webcomponents@2f236870c6768e5744a14568b239eea9cc7bdb30 🚀

--- nightly/local-cdn/base/dist/generated/VersionInfo.js
+++ nightly/local-cdn/base/dist/generated/VersionInfo.js
@@ -5,7 +5,7 @@ const VersionInfo = {
     patch: 0,
     suffix: "-rc.4",
     isNext: false,
-    buildTime: 1771293543,
+    buildTime: 1771380145,
 };
 export default VersionInfo;
 //# sourceMappingURL=VersionInfo.js.map
