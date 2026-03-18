52a3da1c559976c5a660d7ce7ae8473b1a732e51
plamenivanov91 <plamenivanov91@users.noreply.github.com>
2026-03-18 02:07:57 +0000

    Deploying to gh-pages from @ plamenivanov91/ui5-webcomponents@2f236870c6768e5744a14568b239eea9cc7bdb30 🚀

--- nightly/assets/test/pages/theming/Themes2.html-CAGEiqeS.js
+++
@@ -1,1 +0,0 @@
-import"../../../bundle.esm-0dEeSjqe.js";setTimeout(()=>{window["sap-ui-webcomponents-bundle"].configuration.setTheme("sap_horizon_hcb")},1e3);
---
+++ nightly/assets/test/pages/theming/Themes5.html-Cs83fxmB.js
@@ -0,0 +1,1 @@
+import"../../../bundle.esm-CKj6NdXy.js";setTimeout(()=>{window["sap-ui-webcomponents-bundle"].configuration.setTheme("sap_horizon_hcb")},1e3);
