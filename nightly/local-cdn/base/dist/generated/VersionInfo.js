68b99575ff8b22612bb5062150809bff9cf4101c
plamenivanov91 <plamenivanov91@users.noreply.github.com>
2026-06-24 03:28:57 +0000

    Deploying to gh-pages from @ plamenivanov91/ui5-webcomponents@2f236870c6768e5744a14568b239eea9cc7bdb30 🚀

--- nightly/local-cdn/base/dist/generated/VersionInfo.js
+++ nightly/local-cdn/base/dist/generated/VersionInfo.js
@@ -5,7 +5,7 @@ const VersionInfo = {
     patch: 0,
     suffix: "-rc.4",
     isNext: false,
-    buildTime: 1782183536,
+    buildTime: 1782271461,
 };
 export default VersionInfo;
 //# sourceMappingURL=VersionInfo.js.map
--- nightly/local-cdn/main/dist/assets/ListItemStandardExpandableTextTemplate-Bj8hefto.js
+++
@@ -1,1 +0,0 @@
-import{a as x,E as m}from"./bundle.esm-CFj55nVZ.js";function p(a){const{className:t,text:e,maxCharacters:r,part:s}=a;return x(m,{part:s,class:t,text:e,maxCharacters:r})}export{p as default};
